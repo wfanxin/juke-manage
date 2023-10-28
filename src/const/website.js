573dcce9d7d373a6045c6e7804838afec6d8a4bb
Xueg <xueg@ymify.com>
2023-10-28 18:10:42 +0800

    修改网站名称

--- src/const/website.js
+++ src/const/website.js
@@ -1,15 +1,15 @@
 export default {
-  title: '聚客系统',
+  title: '运营管理',
   logo: 'N',
   author: '',
   whiteList: ['/login', '/404', '/401', '/lock'],
   lockPage: '/lock',
   info: {
-    title: '聚客系统',
+    title: '运营管理',
     list: []
   },
   wel: {
-    title: '聚客系统',
+    title: '运营管理',
     list: []
   }
 }
